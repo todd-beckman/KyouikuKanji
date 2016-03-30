8af395339ca34518e93a3697cfd7ce075ebae0aa
Todd Beckman <toddbeckman1@gmail.com>
2016-03-30 08:51:03 -0600

    fix trailing line endings

--- js/script.js
+++ js/script.js
@@ -12,28 +12,28 @@ function loadFile(path, callback, grade) {
     xhttp.send();
 }
 
-var ERROR = "<div class=\"row\">
-    <div class=\"col-sm-12\">
-        Error: &
-    </div>
-</div>";
+var ERROR = "<div class=\"row\">" + 
+"    <div class=\"col-sm-12\">" +
+"        Error: &" +
+"    </div>" +
+"</div>";
 
 function reportError(grade, error) {
     document.getElementById("grade" + grade).innerHTML = ERROR.replace("&", error);
 }
 
 var WIDTHS = [1, 1, 1, 3, 3, 3];
-var HEADER = "<div class=\"row\">
-    <div class=\"col-sm-12\">Grade &</div>
-</div>
-<div class=\"row\">
-    <div class=\"col-sm-" + WIDTHS[0] + "\">ID</div>
-    <div class=\"col-sm-" + WIDTHS[1] + "\">Kanji</div>
-    <div class=\"col-sm-" + WIDTHS[2] + "\">Strokes</div>
-    <div class=\"col-sm-" + WIDTHS[3] + "\">English</div>
-    <div class=\"col-sm-" + WIDTHS[4] + "\">On</div>
-    <div class=\"col-sm-" + WIDTHS[5] + "\">Kun</div>
-</div>";
+var HEADER = "<div class=\"row\">" +
+"    <div class=\"col-sm-12\">Grade &</div>" +
+"</div>" +
+"<div class=\"row\">" +
+"    <div class=\"col-sm-" + WIDTHS[0] + "\">ID</div>" +
+"    <div class=\"col-sm-" + WIDTHS[1] + "\">Kanji</div>" +
+"    <div class=\"col-sm-" + WIDTHS[2] + "\">Strokes</div>" +
+"    <div class=\"col-sm-" + WIDTHS[3] + "\">English</div>" +
+"    <div class=\"col-sm-" + WIDTHS[4] + "\">On</div>" +
+"    <div class=\"col-sm-" + WIDTHS[5] + "\">Kun</div>" +
+"</div>";
 
 
 
@@ -48,25 +48,24 @@ function setGradeContent(grade, csv) {
         if (line.length != 6) {
             reportError("Line " + i + " of grade " + grade + " not properly formatted.");
         }
-        document.getElementById("grade" + grade).innerHTML += "
-            <div class=\"row\">
-                <div class=\"col-sm-" + WIDTHS[0] + "\">" + line[0] + "</div>
-            </div>
-            <div class=\"row\">
-                <div class=\"col-sm-" + WIDTHS[1] + "\">" + line[1] + "</div>
-            </div>
-            <div class=\"row\">
-                <div class=\"col-sm-" + WIDTHS[2] + "\">" + line[2] + "</div>
-            </div>
-            <div class=\"row\">
-                <div class=\"col-sm-" + WIDTHS[3] + "\">" + line[3] + "</div>
-            </div>
-            <div class=\"row\">
-                <div class=\"col-sm-" + WIDTHS[4] + "\">" + line[4] + "</div>
-            </div>
-            <div class=\"row\">
-                <div class=\"col-sm-" + WIDTHS[5] + "\">" + line[5] + "</div>
-            </div>";
+document.getElementById("grade" + grade).innerHTML += "<div class=\"row\">" +
+"   <div class=\"col-sm-" + WIDTHS[0] + "\">" + line[0] + "</div>" +
+"</div>" +
+"<div class=\"row\">" +
+"    <div class=\"col-sm-" + WIDTHS[1] + "\">" + line[1] + "</div>" +
+"</div>" +
+"<div class=\"row\">" +
+"    <div class=\"col-sm-" + WIDTHS[2] + "\">" + line[2] + "</div>" +
+"</div>" +
+"<div class=\"row\">" +
+"    <div class=\"col-sm-" + WIDTHS[3] + "\">" + line[3] + "</div>" +
+"</div>" +
+"<div class=\"row\">" +
+"    <div class=\"col-sm-" + WIDTHS[4] + "\">" + line[4] + "</div>" +
+"</div>" +
+"<div class=\"row\">" +
+"    <div class=\"col-sm-" + WIDTHS[5] + "\">" + line[5] + "</div>" +
+"</div>";
 
     }
 }
