0a7bc24dc02f0da1ff9db852420b1aa0a49e1a2b
Todd Beckman <toddbeckman1@gmail.com>
2016-03-30 08:43:46 -0600

    initial scripting

---
+++ js/script.js
@@ -0,0 +1,77 @@
+var HERE = window.location.hostname + window.location.pathname;
+HERE = substring(0, HERE.length - 12);
+
+function loadFile(path, callback, grade) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            callback(xhttp.responseText, grade);
+        }
+    };
+    xhttp.open("GET", path, true);
+    xhttp.send();
+}
+
+var ERROR = "<div class=\"row\">
+    <div class=\"col-sm-12\">
+        Error: &
+    </div>
+</div>";
+
+function reportError(grade, error) {
+    document.getElementById("grade" + grade).innerHTML = ERROR.replace("&", error);
+}
+
+var WIDTHS = [1, 1, 1, 3, 3, 3];
+var HEADER = "<div class=\"row\">
+    <div class=\"col-sm-12\">Grade &</div>
+</div>
+<div class=\"row\">
+    <div class=\"col-sm-" + WIDTHS[0] + "\">ID</div>
+    <div class=\"col-sm-" + WIDTHS[1] + "\">Kanji</div>
+    <div class=\"col-sm-" + WIDTHS[2] + "\">Strokes</div>
+    <div class=\"col-sm-" + WIDTHS[3] + "\">English</div>
+    <div class=\"col-sm-" + WIDTHS[4] + "\">On</div>
+    <div class=\"col-sm-" + WIDTHS[5] + "\">Kun</div>
+</div>";
+
+
+
+function setGradeHeader(grade) {
+    document.getElementById("grade" + grade).innerHTML = HEADER.replace("&", grade);
+}
+
+function setGradeContent(grade, csv) {
+    var lines = csv.split("\n");
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i].split(",");
+        if (line.length != 6) {
+            reportError("Line " + i + " of grade " + grade + " not properly formatted.");
+        }
+        document.getElementById("grade" + grade).innerHTML += "
+            <div class=\"row\">
+                <div class=\"col-sm-" + WIDTHS[0] + "\">" + line[0] + "</div>
+            </div>
+            <div class=\"row\">
+                <div class=\"col-sm-" + WIDTHS[1] + "\">" + line[1] + "</div>
+            </div>
+            <div class=\"row\">
+                <div class=\"col-sm-" + WIDTHS[2] + "\">" + line[2] + "</div>
+            </div>
+            <div class=\"row\">
+                <div class=\"col-sm-" + WIDTHS[3] + "\">" + line[3] + "</div>
+            </div>
+            <div class=\"row\">
+                <div class=\"col-sm-" + WIDTHS[4] + "\">" + line[4] + "</div>
+            </div>
+            <div class=\"row\">
+                <div class=\"col-sm-" + WIDTHS[5] + "\">" + line[5] + "</div>
+            </div>";
+
+    }
+}
+
+for (var i = 1; i < 7; i++) {//OBOE
+    setGradeHeader(i);
+    loadFile(HERE + i + ".csv", setGradeContent, i);
+}
